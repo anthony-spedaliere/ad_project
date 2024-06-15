c3cc46f7a7e60c5e88fe231ff9d0d612907c06ad
Anthony Spedaliere <avspedaliere@gmail.com>
2024-06-15 12:31:01 -0700

    added globalstyles, styled-components, redux and react-query to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import GlobalStyles from "./styles/GlobalStyles";
+
+// Create query client for react-query
+const queryClient = new QueryClient({
+  queries: {
+    staleTime: 60 * 1000,
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyles />
+      <ReactQueryDevtools />
       <h1>TEST</h1>
-    </>
+    </QueryClientProvider>
   );
 }
 
