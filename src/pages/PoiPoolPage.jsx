f4668b0b28a6bead3493b7f20a58d94946da4c2c
Anthony Spedaliere <avspedaliere@gmail.com>
2024-09-05 17:35:27 -0700

    Implemented the left sidebar styling and main area styling

--- src/pages/PoiPoolPage.jsx
+++ src/pages/PoiPoolPage.jsx
@@ -2,6 +2,51 @@ function PoiPoolPage() {
   return (
     <div>
       <div>Poi Pool Page</div>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
+      <h1>Poi Pool</h1>
     </div>
   );
 }
