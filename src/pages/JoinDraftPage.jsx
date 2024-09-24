7267aacfc5b54c9e7624098281ee4a2db6c613fb
Anthony Spedaliere <avspedaliere@gmail.com>
2024-09-24 13:09:00 -0700

    moved JoinDraftPage styles to a separate file

--- src/pages/JoinDraftPage.jsx
+++ src/pages/JoinDraftPage.jsx
@@ -13,101 +13,19 @@ import {
   setTeamsHaveJoined,
   setTeamTurnList,
 } from "../store/slices/liveDraftSlice";
+import {
+  HeaderContent,
+  Main,
+  Container,
+  Header,
+  Title,
+  BackButton,
+  AdminJoinDraftButton,
+  TeamsContainer,
+  CountdownBoxStyle,
+} from "../styles/JoinDraftPageStyles";
 import CustomCountdownBox from "../components/CustomCountdownBox";
 import { useEffect, useMemo } from "react";
-
-const Main = styled.main`
-  background-color: var(--background-color);
-  padding: 4rem 4.8rem 6.4rem;
-  overflow: auto;
-  height: 100vh;
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  max-width: 140rem;
-  margin: 0 auto;
-  gap: 3.2rem;
-`;
-
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-  background-color: var(--background-color);
-  margin-bottom: 5rem;
-`;
-
-const Title = styled.h1`
-  font-size: 4rem;
-  color: var(--brand-color);
-`;
-
-const HeaderContent = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-export const BackButton = styled.button`
-  color: ${(props) => props.$customColor || "var(--red-color)"};
-  text-decoration: none;
-  font-size: 2rem;
-  background: none;
-  border: none;
-  cursor: pointer;
-  transition: color 0.3s ease;
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  margin-bottom: ${(props) => props.$mgBottom || "0rem"};
-
-  &:hover {
-    color: var(--color-grey-400);
-  }
-`;
-
-const AdminJoinDraftButton = styled.button`
-  color: ${(props) => props.$customColor || "var(--blue-color)"};
-  text-decoration: underline;
-  font-size: 2rem;
-  background: none;
-  border: none;
-  cursor: pointer;
-  transition: color 0.3s ease;
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  margin-bottom: ${(props) => props.$mgBottom || "0rem"};
-
-  &:hover {
-    color: var(--color-grey-400);
-  }
-`;
-
-const TeamsContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-  padding: 1rem;
-  gap: 1rem;
-  max-width: 100%;
-`;
-
-const CountdownBoxStyle = styled.div`
-  background-color: var(--brand-color);
-  min-height: 13rem;
-  min-width: 16rem;
-  padding: 0.5rem;
-  color: var(--background-color);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: 2rem;
-  margin-left: 2rem;
-  border-radius: 1rem;
-`;
 
 function JoinDraftPage() {
   const navigate = useNavigate();
